f82d6e57b4454cdcb978228f974ad1eba24e8fe8
Giulia <gbeleca@protonmail.com>
2019-10-26 19:33:15 +0200

    I've made the corrections and assume it should  work well.

--- Currency_converter.go
+++ Currency_converter.go
@@ -1,14 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-const dollarRate = 63e68
-
-func getResult() {
+func main() {
 	fmt.Println("Конвертер валют из Р в $. Введите интересующую вас сумму рублей в окно, чтобы получить эквивалент в долларах.")
-	fmt.Scanln(&rub)
-	result = rub / dollarRate
-	fmt.Println("Отдав ", rub, " рублей, вы получите ", result, " $. По курсу ", dollarRate, " рублей за доллар.")
+	var rur float64
+	fmt.Scanln(&rur)
+
+	const dollarRate float64 = 63.68
+
+	var result float64 = rur / dollarRate
+
+	fmt.Printf("Отдав %v рублей, вы получите %.2f $. По курсу %v рублей за доллар.", rur, result, dollarRate)
 }
