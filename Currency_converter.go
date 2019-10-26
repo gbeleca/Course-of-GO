66239b7a55c040cadb5bf1bf3bf0de9bc7af24fb
Talisman <a.egorov@nativetech.ru>
2019-10-26 19:39:29 +0300

    fix конвертора с комментариями

--- Currency_converter.go
+++ Currency_converter.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 )
 
-const dollarRate = 63e68
+const dollarRate = 63
 
-func getResult() {
+func main() {
+	//rub перед использованием любую переменную нужно объявить
+	var rub float64
 	fmt.Println("Конвертер валют из Р в $. Введите интересующую вас сумму рублей в окно, чтобы получить эквивалент в долларах.")
 	fmt.Scanln(&rub)
-	result = rub / dollarRate
+	//если мы хотим присвоить значение переменной без предварительного объявления, нужно использовать конструкцию :=
+	result := rub / dollarRate
 	fmt.Println("Отдав ", rub, " рублей, вы получите ", result, " $. По курсу ", dollarRate, " рублей за доллар.")
 }
