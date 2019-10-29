d3cde9879aec727595f007fedd759b2f471e0acb
Giulia <gbeleca@protonmail.com>
2019-10-29 05:06:36 +0100

    Revert "Revert "Revert "Merge remote-tracking branch 'course-of-GO/master'"""
    This reverts commit f536cdff731c0ebe4a9a09faddedba6963ea7b66.

--- 2nd lesson/Devision_on_3.go
+++ 2nd lesson/Devision_on_3.go
@@ -1,17 +1,16 @@
-package 2nd lesson
+package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main(userNumber) {
 	for i := 0; i <= 10; i++ {
 		fmt.Println("Введите число, чтобы узнать является ли оно четным:")
-			var userNumber int
-			fmt.Scanln(&userNumber)
-			var residue int = userNumber % 3
-		if userNumber % 3 == 0 {
+		var userNumber int
+		fmt.Scanln(&userNumber)
+		var residue int = userNumber % 3
+		if userNumber%3 == 0 {
 			fmt.Println("Это число делится без остатка на 3.")
 		} else {
 			fmt.Println("Это число не делится на 3, остаток:", residue)
